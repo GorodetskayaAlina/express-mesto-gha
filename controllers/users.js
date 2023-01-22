0cb3c8a5a95721307d1dabff89b70581c76085a6
Alina Gorodetskaya <gorodetskaya_alina1998@mail.ru>
2023-01-22 21:53:11 +0300

    14 спринт ревью валидация

--- controllers/users.js
+++ controllers/users.js
@@ -62,10 +62,6 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      if (!email || !password) {
-        next(new UnauthorizedError('Неправильные почта или пароль'));
-      }
-
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
       return res.send({ token });
     })
